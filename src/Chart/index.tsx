52d6f1b11fa7687d5ce59c5d2c7ff94782b35c1d
Leonardo Quixadá <195494+lquixada@users.noreply.github.com>
2026-03-02 09:17:59 -0500

    printed values for debugging

--- src/Chart/index.tsx
+++ src/Chart/index.tsx
@@ -1,7 +1,15 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import React, { Suspense } from 'react'
 import Title from './Title';
 
 const Plot = React.lazy(() => import('./Plot'));
+
+/* Printing values for debugging */
+console.log(import.meta.url);
+console.log(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname);
 
 const SimpleChart = () => {
   return (
